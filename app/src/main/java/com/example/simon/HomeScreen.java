1338b970be1d7a4686c5ce36f4554a3d1982a460
Sam Herron <samdherron@gmail.com>
2019-11-11 20:43:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simon/HomeScreen.java
@@ -0,0 +1,29 @@
+package com.example.simon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreen extends AppCompatActivity
+        implements View.OnClickListener {
+
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+        btnStart = findViewById(R.id.btnStartGame);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent gameScreen = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(gameScreen);
+    }
+}
